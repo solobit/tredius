03b85e9f9d27c7dc1be3efe6dea2eb98576b057b
supersymmetry <maildumpert@gmail.com>
2013-06-11 18:20:21 +0200

    update

--- project.clj
+++ project.clj
@@ -1,11 +1,28 @@
 (defproject
+
   nl.tredius.portal
   "0.1.0-SNAPSHOT"
+
+  :min-lein-version "2.0.0"
+
+  :url "http://tredius.nl"
+
+  :main nl.tredius.portal.core
+
+  :description
+  "Tredius next-gen web portal made by Solobit."
+
+  :plugins
+  [;[lein-ring "0.8.5"] ; using http-kit
+   [lein-cljsbuild "0.3.2"]]
+
   :dependencies
   [[org.clojure/clojure "1.5.1"]
+   [clj-tagsoup "0.3.0"]
+   [http-kit "2.1.3"]
    [lib-noir "0.6.1"]
    [compojure "1.1.5"]
-   [ring-server "0.2.8"]
+   ;[ring-server "0.2.8"] ; using http-kit
    [clabango "0.5"]
    [com.taoensso/timbre "2.1.2"]
    [com.postspectacular/rotor "0.1.0"]
@@ -24,7 +41,8 @@
      javax.jms/jms
      com.sun.jdmk/jmxtools
      com.sun.jmx/jmxri]]
-   [http-kit "2.1.3"]]
+   ]
+
   :cljsbuild
   {:builds
    [{:source-paths ["src-cljs"],
@@ -32,22 +50,17 @@
      {:pretty-print false,
       :output-to "resources/public/js/site.js",
       :optimizations :advanced}}]}
+
   :ring
   {:handler nl.tredius.portal.handler/war-handler,
    :init nl.tredius.portal.handler/init,
    :destroy nl.tredius.portal.handler/destroy}
+
   :profiles
   {:production
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}},
    :dev
    {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.1.8"]]}}
-  :url
-  "http://example.com/FIXME"
-  :main
-  nl.tredius.portal.core
-  :plugins
-  [[lein-ring "0.8.5"] [lein-cljsbuild "0.3.2"]]
-  :description
-  "FIXME: write description"
-  :min-lein-version "2.0.0")
+
+  ) ;; FIN
