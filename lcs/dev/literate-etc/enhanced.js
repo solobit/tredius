b75d077f138a6531936bbd73f7dc90111dd76333
supersymmetry <maildumpert@gmail.com>
2013-06-27 16:59:20 +0200

    removed lcs-dev

--- lcs/dev/literate-etc/enhanced.js
+++
@@ -1,8 +0,0 @@
-var myRequire = require("enhanced-require")(module, {
-    // options
-    recursive: true // enable for all modules recursivly
-    // This replaces the original require function in loaded modules
-});
-
-// startup your application
-myRequire("startup.js");
